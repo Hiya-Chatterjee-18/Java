1cfc3f3b8046937e383fd26832fe60d4d4bfe92e
HIYA CHATTERJEE <hiya2005.chat@gmail.com>
2025-04-12 17:56:30 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class three{
+    public static void main(String[] args) {
+        System.out.println("Enter an input");
+        Scanner sc=new Scanner(System.in);
+        boolean b=sc.hasNextInt();
+        System.out.println(b);
+    }
+}
