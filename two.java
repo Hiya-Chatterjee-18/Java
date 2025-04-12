c4fddc2ac2389acd7858dc1eed37a7ebbd96375b
HIYA CHATTERJEE <hiya2005.chat@gmail.com>
2025-04-12 17:56:09 +0530

    Add files via upload

---
+++ two.java
@@ -0,0 +1,21 @@
+
+public class two {
+    public static void main(String[] args) {
+        byte age=34;
+        int age2=56;
+        short age3=87;
+        long age4=8568356923569l;
+        char ch='H';
+        float f1=5.4f;
+        double d1=46.48;
+        boolean con=true;
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(con);
+    }
+}
