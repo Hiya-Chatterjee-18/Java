25658f4376560cb3dbfa223fec8f81d74cbdc440
HIYA CHATTERJEE <hiya2005.chat@gmail.com>
2025-04-12 17:55:33 +0530

    Add files via upload

---
+++ One.java
@@ -0,0 +1,14 @@
+
+import java.lang.*;
+import java.util.*;
+
+class One
+{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String name;
+        System.err.println("May I Know Your Name?");
+        name=s.nextLine();
+        System.err.println("Welcome Mr./Miss "+name);
+    }
+}
