bd4b2094f194b254f71c560ae7c6c738666b324f
HIYA CHATTERJEE <hiya2005.chat@gmail.com>
2025-04-17 12:19:17 +0530

    Add files via upload

---
+++ four.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class four{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        float a=sc.nextFloat();
+        System.out.println("Enter 2nd number:");
+        float b=sc.nextFloat();
+        float c=a+b;
+        System.out.println("The sum is :"+c);
+    }
+} 
